e97184924c5a839a74bb4a79a2b470bd980d9ecb
Mendoza-Kevin <kevingfx71@gmail.com>
2026-01-07 20:38:36 -0600

    feat: Add initial Alembic migration for productos, categorias, usuarios

--- app/api/v1/categorias.py
+++ app/api/v1/categorias.py
@@ -6,19 +6,19 @@ from crud.categoria import *
 
 api_router = APIRouter()
 
-@api_router.get("/categorias", response_model=list[CategoriaResponse])
+@api_router.get("/", response_model=list[CategoriaResponse])
 def listar_categorias(db:Session = Depends(get_db)):
     return obtener_categorias(db)
 
-@api_router.post("/categorias", response_model=CategoriaCreate)
+@api_router.post("/", response_model=CategoriaCreate)
 def agregar_categoria(categoria: CategoriaCreate, db:Session = Depends(get_db)):
     return crear_categoria(db, categoria)
 
-@api_router.put("/categorias/{id}", response_model=CategoriaCreate)
+@api_router.put("/{id}", response_model=CategoriaCreate)
 def actualizar_categoria(categoria_id: int, datos: CategoriaCreate , db:Session = Depends(get_db)):
     return actualizar_categoria(db, categoria_id, datos)
 
-@api_router.delete("/categorias/{id}")
+@api_router.delete("/{id}")
 def eliminar_categoria(categoria_id: int, db:Session = Depends(get_db)):
     eliminar_categoria(db, categoria_id)
     return {"mensaje":"Categoria eliminada correctamente"}
--- app/api/v1/productos.py
+++ app/api/v1/productos.py
@@ -6,19 +6,19 @@ from crud.producto import *
 
 api_router = APIRouter()
 
-@api_router.get("/productos", response_model=list[ProductoResponse])
+@api_router.get("/", response_model=list[ProductoResponse])
 def listar_productos(db:Session = Depends(get_db)):
     return obtener_productos(db)
 
-@api_router.post("/productos", response_model=ProductoCreate, dependencies=[Depends(require_admin)])
+@api_router.post("/", response_model=ProductoCreate, dependencies=[Depends(require_admin)])
 def agregar_producto(producto: ProductoCreate, db: Session = Depends(get_db)):
     return crear_producto(db, producto)
 
-@api_router.put("/productos/{id}", response_model=ProductoCreate)
+@api_router.put("/{id}", response_model=ProductoCreate)
 def actualizar_producto(producto_id: int, datos: ProductoCreate, db:Session = Depends(get_db)):
     return actualizar_producto(db, producto_id, datos)
 
-@api_router.delete("/productos/{id}")
+@api_router.delete("/{id}")
 def eliminar_producto(producto_id = int, db:Session = Depends(get_db)):
     eliminar_producto(db, producto_id)
     return {"mensaje":"Producto eliminado correctamente"}
